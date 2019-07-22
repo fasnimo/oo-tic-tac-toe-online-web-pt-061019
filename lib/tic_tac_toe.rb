e104e28733aa6f70123f8a95b214c9c7096422c4
Rene Cocom <fasnimo@gmail.com>
2019-07-22 22:44:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ def turn
     index = input_to_index(user_input)
     
     if valid_move?(index)
-      move(index, token)
+      move(index, current_player)
        display_board
     else
       turn
